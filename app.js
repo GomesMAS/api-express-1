bbf0b11b0f1514d9b79319fbaa8d940c5e78df2f
GomesMAS <161360739+GomesMAS@users.noreply.github.com>
2026-09-17 13:43:59 -0400

    app.js
    resoluçao de exercicios

--- app.js
+++ app.js
@@ -6,76 +6,68 @@ app.use(express.json());
 
 const PORT = 3000;
 
-const produtos = [
-  { id: 1, nome: "Teclado", categoria: "periféricos" },
-  { id: 2, nome: "Controle", categoria: "periféricos" },
-  { id: 3, nome: "Mouse", categoria: "periféricos" },
-  { id: 4, nome: "Processador AMD Ryzen 7 7500", categoria: "eletrônicos" },
+const tarefas = [
+  { id: 1, titulo: "Matematica", concluida: "true" },
+  { id: 2, titulo: "engenharia", concluida: "true" },
+  { id: 3, titulo: "fisica", concluida: "true" },
+  { id: 4, titulo: "ingles", concluida: "false" },
 ];
 
 app.get("/", (req, res) => {
-  res.send("Olá Express!");
+  res.send("API de tarefas no ar!");
 });
 
-app.get("/produtos", (req, res) => {
-  res.json(produtos);
+app.get("/tarefas", (req, res) => {
+  res.json(tarefas);
+});
+app.get("/tarefas/:id", (req, res) => {
+  const id = req.params.id;
+  const tarefaencontrada = tarefas.find((p) => p.id === parseInt(id));
+  if (!tarefas) {
+    return res.status(404).json({ error: "tarefa não encontrado!" });
+  }
+  res.status(200).json(tarefaencontrada);
 });
 
-app.post("/produto", (req, res) => {
-  const nome = req.body.nome;
-  const categoria = req.body.categoria;
+app.post("/tarefas", (req, res) => {
+  const titulo = req.body.titulo;
+  const concluida = req.body.concluida;
 
-  if (!nome || typeof nome !== "string") {
+  if (!titulo || typeof titulo !== "string") {
     return res
       .status(400)
-      .json({ erro: 'O campo "nome" é obrigatório e deve ser uma string' });
+      .json({ erro: 'O campo "titulo" é obrigatório e deve ser uma string' });
   }
 
-  if (categoria === undefined || typeof categoria !== "string") {
+  if (concluida === undefined || typeof concluida !== Boolean) {
     return res
       .status(400)
       .json({
-        erro: 'O campo "categoria" é obrigatório e deve ser uma string',
+        erro: 'O campo "concluida" é obrigatório e deve ser um booleano ',
       });
   }
 
-  const novoProduto = {
-    id: produtos.length + 1,
-    nome,
-    categoria,
+  const novatarefa = {
+    id:tarefas.length + 1,
+    titulo,
+    concluida,
   };
 
-  produtos.push(novoProduto);
+tarefas.push(novatarefa);
 
-  res.status(201).json(novoProduto);
+  res.status(201).json(novatarefa);
 });
 
-// Req Params, pega um parâmetro na rota e identifica um recurso específico;
-app.get("/produtos/:id", (req, res) => {
-  // req.params sempre retorna strings, por isso se usa parseInt
-  const id = req.params.id;
-  const produto = produtos.find((p) => p.id === parseInt(id));
-  if (!produto) {
-    return res.status(404).json({ error: "Produto não encontrado!" });
-  }
-  res.status(200).json(produto);
-});
 
-app.get("/produtos/busca", (req, res) => {
-  const categoria = req.query.categoria;
+app.get("tarefas/busca", (req, res) => {
+  const concluida = req.query.concluida;
   const ordenar = req.query.ordenar;
 
-  let resultado = produtos;
+  let resultado =tarefas;
 
-  if (categoria) {
+  if (concluida) {
     resultado = resultado.filter((produto) => {
-      produto.categoria === categoria;
-    });
-  }
-
-  if (ordenar) {
-    resultado = [...resultado].sort((a, b) => {
-      a.preco - b.preco;
+      produto.concluida === true;
     });
   }
 
